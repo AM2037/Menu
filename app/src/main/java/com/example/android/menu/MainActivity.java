5954728c18116a4d258bfe7ed34688ab70745487
Amber <adm2037@gmail.com>
2018-03-23 08:08:06 -0500

    Used getter methods to log

--- app/src/main/java/com/example/android/menu/MainActivity.java
+++ app/src/main/java/com/example/android/menu/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.android.menu;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,12 +15,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     *Using getter method getText, then another method toString to convert the result
+     * which is a CharSequence returned from getText into a String. Use Log.v to print
+     * which is the verbose method (there are 5 different types for logcat-- See docs.
+     */
     public void printToLogs(View view) {
         // Find first menu item TextView and print the text to the logs
-
+        TextView textViewItem1 = (TextView) findViewById(R.id.menu_item_1);
+        String menuItem1 = textViewItem1.getText().toString();
+        Log.v("MainActivity", menuItem1);
         // Find second menu item TextView and print the text to the logs
-
+        TextView textViewItem2 = (TextView) findViewById(R.id.menu_item_2);
+        String menuItem2 = textViewItem2.getText().toString();
+        Log.v("MainActivity", menuItem2);
         // Find third menu item TextView and print the text to the logs
-
+        TextView textViewItem3 = (TextView) findViewById(R.id.menu_item_3);
+        String menuItem3 = textViewItem3.getText().toString();
+        Log.v("MainActivity", menuItem3);
     }
 }
